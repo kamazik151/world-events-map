0ef1d298de799db76943d181acc0a03df92ea745
kamazik151 <tymofiy.skrebets@gmail.com>
2026-03-08 17:36:08 +0200

    Initialize map after DOM content is loaded
    Wrap map initialization in DOMContentLoaded event listener.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var map = L.map('map').setView([20,0],2);
+document.addEventListener("DOMContentLoaded", function() {
+    var map = L.map('map').setView([20,0],2);
 
-L.tileLayer(
-'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
+
+});
 
 let markers = [];
 
@@ -172,5 +173,3 @@ L.marker([lat,lon])
 });
 
 }
-
-loadNews();
